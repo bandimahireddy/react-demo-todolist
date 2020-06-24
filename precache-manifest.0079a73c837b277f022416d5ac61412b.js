370f52f2a0e154cb9394be15c68e4c6eb370e03b
Mahendranath Reddy <mahi.bandireddynath@gmail.com>
2020-06-24 09:08:38 +0530

    Updates

---
+++ precache-manifest.0079a73c837b277f022416d5ac61412b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95c2cb3fef745a3cb997c03120870bc",
+    "url": "/react-demo-todolist/index.html"
+  },
+  {
+    "revision": "9a623564367c99a0307e",
+    "url": "/react-demo-todolist/static/css/main.8d5a0a4e.chunk.css"
+  },
+  {
+    "revision": "bd0c97b7a831b9122b2d",
+    "url": "/react-demo-todolist/static/js/2.0512a152.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-demo-todolist/static/js/2.0512a152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a623564367c99a0307e",
+    "url": "/react-demo-todolist/static/js/main.579101d3.chunk.js"
+  },
+  {
+    "revision": "d6816a91661d60a4d6b8",
+    "url": "/react-demo-todolist/static/js/runtime-main.b33dce0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-demo-todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
